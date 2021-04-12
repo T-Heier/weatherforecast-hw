0bfb707c8297002de405b477ed1b8430cce60767
T-Heier <trentheier@gmail.com>
2021-04-12 19:14:21 -0400

    added functions to append the data to the page

--- script.js
+++ script.js
@@ -2,33 +2,72 @@ var searchBtn = document.getElementById("buttonSearch")
 
 searchBtn.onclick = function(){
     var cityName = document.getElementsByClassName("cityInput")[0].value
-    console.log(cityName)
+    
+    fiveDay(cityName)
+    getWeather(cityName)
+}
+
+function fiveDay(cityName) {
+
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + key)
+    .then(function(response) {
+        return response.json()})
+    .then(function (data) {
+        createCards(data)
+    })
+}
+
+function getWeather(cityName) {
+
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key)
     .then(function(response) {
         return response.json()})
     .then(function (data) {
-        console.log(data)
+        appendData(data)
         var lat = data.coord.lat
         var lon = data.coord.lon
         fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${key}`)
         .then(function (response) {
             return response.json()})
         .then(function (data) {
-            console.log(data)
+            UV(data)
         })
-        // value = uv value
     })
-    fiveDay(cityName)
+}
+// this is where my data should append to
+const cityName = document.getElementsByClassName("cityName")
+const cityInfo = document.getElementsByClassName("cityWeather")
+
+function appendData(data) {
+    let createH1 = document.createElement("h1")
+    let cityWeather = [data.main.temp, data.main.humidity, data.wind.speed, data.name]
+    var createLi;
+    cityName[0].appendChild(createH1)
+    createH1.textContent = cityWeather[3]
+
+    for (i = 0; i < cityWeather.length - 1; i++ ) {
+        createLi = document.createElement("li")
+        createLi.textContent = cityWeather[i]
+        cityInfo[0].appendChild(createLi);
+    }
+    cityInfo[0].appendChild(createLi)
 }
 
-function fiveDay(cityName) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + key)
-    .then(function(response) {
-        return response.json()})
-    .then(function (data) {
-    console.log(data)
-    })
+function UV(data) {
+    console.log(data.value)
+    var createLi;
+    createLi = document.createElement("li")
+    createLi.textContent = data.value
+    cityInfo[0].appendChild(createLi);
 }
+
+function createCards(data) {
+    console.log(data)
+}
+
+
+
+
 
 
 // for loop to look for specific time of day for 5 day forecast
